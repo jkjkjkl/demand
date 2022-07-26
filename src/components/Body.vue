3bc7f4512b4c10119f9688deb408a49f4950fcf1
xjk <1518472614@qq.com>
2022-07-26 16:38:07 +0800

    first commit

---
+++ src/components/Body.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="body">
+        <div class="piteimg">
+        <img src=".././assets/img/流量使用.jpg" alt="1"  class="img-1">
+        <img src=".././assets/img/空间使用.jpg" alt="2">
+         <img src=".././assets/img/系统公告.jpg" alt="5">       
+        <img src=".././assets/img/7天流量.jpg" alt="4">
+        <img src=".././assets/img/7天播放.jpg" alt="3">
+        <img src=".././assets/img/账号信息.jpg" alt="6">
+        
+    </div>
+    </div>
+</template>
+<style scoped>
+    img{
+        width:427px;
+        /* height:560px; */
+    }
+</style>
+
+ 
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div class="nav-background"> 
+        <div  class="logo">
+            <img src="../assets/img/logo.png" alt="" > 
+        </div>  
+        <div class="header">
+            <li><a href="https://my.polyv.net/secure/main" class="qaq">云点播</a>
+                <img src="/img/箭头.png" alt="" class="logo2">
+                <ul>
+                    <li><a href="https://console.polyv.net/home/">产品首页</a></li>
+                    <li><a href="https://console.polyv.net/home/" style="color:rgb(33, 150, 243)">云点播</a></li>
+                    <li><a href="https://console.polyv.net/live/#/channel">云直播</a></li>
+                    <li><a href="https://portal.polyv.net/index">视频微门户</a></li>
+                </ul>
+            </li>
+        
+        </div>
+        <div class="nav1">
+            <ul>               
+                <li class="overview"><a href="https://my.polyv.net/secure/main" >概览</a></li>
+                <li><a href="https://my.polyv.net/secure/uploadvideochunk_v2">上传视频</a></li>
+                <li><a href="https://my.polyv.net/secure/video/">视频列表</a></li>
+                <li><a href="https://my.polyv.net/secure/player">播放器设置</a></li>
+                <li><a href="https://my.polyv.net/secure/advertising/">广告管理</a></li>
+                <li><a href="https://data.polyv.net/secure/overview/?lang=zh_CN">数据统计</a></li>
+                
+            </ul>
+        </div>  
+        <div class="setting">
+            <li><a href="">设置</a>
+                <ul>
+                    <li><a href="https://my.polyv.net/secure/setting/callback">回调设置</a></li>
+                    <li><a href="https://my.polyv.net/secure/setting/api">API接口 </a> </li>
+                    <li><a href="https://my.polyv.net/secure/setting/account/">账号管理</a></li>
+                    <li><a href="https://my.polyv.net/secure/setting/playsafe">加密设置</a></li>
+                    <li><a href="https://my.polyv.net/secure/setting/video">视频设置</a></li>
+                    <li><a href="https://my.polyv.net/secure/setting/host_setting">播放域名设置</a></li>
+                    <li><a href="https://my.polyv.net/secure/setting/questionnaire">访客信息收集</a></li>
+                    <li><a href="https://my.polyv.net/secure/setting/wechat-share">微信分享设置</a></li>
+                </ul> 
+            </li>
+        </div>  
+        <div class="nav2">
+            <li class="help"><a href=""><img src="../assets/img/帮助.png" alt=""></a></li>
+            <li class="download"><a href=""><img src="../assets/img/下载1.png" alt=""></a></li>
+            <li class="data"><a href=""><img src="../assets/img/数据库,数据.png" alt=""></a></li>
+            <li><a href="" >中文</a>
+                <ul>
+                    <li><a href="">中文</a></li>
+                    <li><a href="">English</a></li>
+                </ul>
+            </li>          
+        </div>     
+    </div> 
+    <div class="test"></div>
+    
+    </div>
+</template>
